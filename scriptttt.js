97c42c0c727c612876a00667e45c265b299d24b7
Fawwaz <98513981+Mdfawwaz12@users.noreply.github.com>
2023-09-30 22:50:49 +0530

    Add files via upload

---
+++ scriptttt.js
@@ -0,0 +1,74 @@
+console.log("spotify")
+
+// initializing the variables
+let songIndex = 0
+let audioElement = new Audio('bgm/main.mp3')
+let masterplay = document.getElementById('masterplay')
+let myrange = document.getElementById('myrange')
+let gif = document.getElementById('gif')
+let songitem = Array.from(document.getElementsByClassName('songitem'))
+
+let songs = [
+    {songName : "Main Banda E aasi Hoon", filepath: "bgm/main.mp3", coverpath:"pic.jpg"},
+    {songName : "Main bhi roza rakhonga", filepath: "bgm/main bhi roza.mp3", coverpath:"img2.jpg"},
+    {songName : "ya aan sallai", filepath: "bgm/ya maan salai.mp3", coverpath:"img1.jpg"},
+    {songName : "sample", filepath: "bgm/sample.mp3", coverpath:"img1.jpg"},
+    {songName : "Alone", filepath: "bgm/master2.mp3", coverpath:"img1.jpg"},
+]
+
+// songitem.forEach((element,i)=> {
+//     // console.log(element ,i)
+//     element.getElementsByTagName("img")[0].src = songs[i].coverpath
+//     element.getElementsByClassName("songsname")[0].innerText = songs[i].songName
+    
+// })
+// Handling the play/pause click
+masterplay.addEventListener('click', ()=>{
+    if(audioElement.paused || audioElement.currentTime<=0){
+        audioElement.play();
+        masterplay.classList.remove('fa-play-circle');
+        masterplay.classList.add('fa-pause-circle');
+        gif.style.opacity = 1;
+    }
+    else{
+        audioElement.pause();
+        masterplay.classList.remove('fa-pause-circle');
+        masterplay.classList.add('fa-play-circle');
+        gif.style.opacity = 0;
+    }
+})
+
+
+// Listening to Event
+audioElement.addEventListener('timeupdate',()=>{
+    // console.log("add event")
+//     // updaing seekbars
+//     progress = parseInt((audioElement.currentTime/audioElement.duration)*100)
+//     // console.log(progress)
+//     myrange.value = progress
+// })
+
+// myrange.addEventListener('change',()=>{
+//     audioElement.currentTime = myrange.value*audioElement.duration/100
+// })
+
+// const allplays=()=>{
+//     Array.from(document.getElementsByClassName('songitemclass')).forEach((element)=>{
+//         element.classList.remove('fa-circle-pause')
+//         element.classList.add('fa-circle-play')
+//     })
+// } 
+
+// Array.from(document.getElementsByClassName('songitemclass')).forEach((element)=>{
+//     element.addEventListener('click', (e)=>{
+//         allplays()
+//         songIndex = parseInt(e.target.id)
+//         e.target.classList.remove('fa-circle-play')
+//         e.target.classList.add('fa-circle-pause')
+//         audioElement.src = `bgm/${songIndex+1}.mp3`
+//         audioElement.currentTime=0
+//         audioElement.play()
+//         masterplay.classList.remove('fa-circle-play')
+//         masterplay.classList.add('fa-circle-pause')
+//     })
+})
